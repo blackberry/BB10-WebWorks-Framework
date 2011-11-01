e2be569ad243e57c5bacbbb0dc07ae448af6831a
Eric Li <eli@rim.com>
2011-11-01 17:17:08 -0400

    Added correct sample CustomData file and updated the unit test accordingly

--- test/unit/config.js
+++ test/unit/config.js
@@ -1,6 +1,6 @@
 var srcPath = __dirname + '/../../lib/';
 
-describe("Config", function() {
+describe("Config", function () {
 	var config = require(srcPath + 'config/Config.js');
 
 /*
@@ -15,26 +15,26 @@ describe("Config", function() {
 
 	}
 */
-	it("verify all testing config data", function() {
+	it("verify all testing config data", function () {
 		expect(config.configXML).toEqual("config.xml");
 		expect(config.configXMLDoc).toEqual(null);
 		expect(config.backButtonBehavior).toEqual("back");
 		expect(config.customHeaders).toBeDefined();
-		expect(config.version).toEqual("1.0.0");
+		expect(config.version).toEqual("1.0.0.0");
 		expect(config.author).toEqual("Me");
-		expect(config.authorURL).toEqual("");
-		expect(config.authorEmail).toEqual("");
-		expect(config.copyright).toEqual("");
-		expect(config.content).toEqual("index.html");
+		expect(config.authorURL).toEqual("http://bbtools_win7_01/yui");
+		expect(config.authorEmail).toEqual("guocat@gmail.com");
+		expect(config.copyright).toEqual("@Rebecca");
+		expect(config.content).toEqual("main.htm");
 		expect(config.contentCharset).toEqual("");
 		expect(config.contentType).toEqual("");
-		expect(config.description).toEqual("");
-		expect(config.icon).toEqual("AIRApp_72.png");
+		expect(config.description).toEqual("this is local");
+		expect(config.icon).toEqual("img/4_colorful_bears.png");
 		expect(config.iconHover).toEqual("");
 		expect(config.id).toEqual("");
 		expect(config.license).toEqual("");
 		expect(config.licenseURL).toEqual("");
-		expect(config.name).toEqual("File Transfer");
+		expect(config.name).toEqual("flTimeout");
 		expect(config.navigationMode).toEqual("pointer");
 		expect(config.preferredTransports).toEqual(null);
 		expect(config.transportTimeout).toEqual(300000);
@@ -42,24 +42,24 @@ describe("Config", function() {
 		expect(config.widgetExtensions).toBeDefined();
 		expect(config.featureTable).toEqual(null);
 		expect(config.accessList).toBeDefined();
-		expect(config.loadingScreenColor).toEqual("#FFFFFF");
+		expect(config.loadingScreenColor).toEqual("#8080FF");
 		expect(config.backgroundImage).toEqual("");
-		expect(config.foregroundImage).toEqual("");
+		expect(config.foregroundImage).toEqual("img/4_colorful_bears.gif");
 		expect(config.onFirstLaunch).toEqual(false);
 		expect(config.onLocalPageLoad).toEqual(false);
 		expect(config.onRemotePageLoad).toEqual(false);
-		expect(config.transitionType).toEqual(-1);
-		expect(config.transitionDuration).toEqual(250);
+		expect(config.transitionType).toEqual(0);
+		expect(config.transitionDuration).toEqual(1000);
 		expect(config.transitionDirection).toEqual(128);
-		expect(config.disableAllCache).toEqual(false);
+		expect(config.disableAllCache).toEqual(true);
 		expect(config.aggressiveCacheAge).toEqual(2592000);
 		expect(config.maxCacheSizeTotal).toEqual(1024);
 		expect(config.maxCacheSizeItem).toEqual(128);
 		expect(config.maxStandardCacheAge).toEqual(2592000);
 		expect(config.runOnStartUp).toEqual(false);
-		expect(config.allowInvokeParams).toEqual(false);
+		expect(config.allowInvokeParams).toEqual(true);
 		expect(config.backgroundSource).toEqual("");
-		expect(config.foregroundSource).toEqual("index.html");
+		expect(config.foregroundSource).toEqual("main.htm");
 		expect(config.debugEnabled).toEqual(false);
 	});
 });
