82c66f915790ebf6852bd64fc78c699050ee0b80
Rosa Tse <rtse@rim.com>
2011-11-03 13:42:47 -0400

    Added test for local protocol

--- test/unit/whitelist.js
+++ test/unit/whitelist.js
@@ -44,7 +44,27 @@ describe("whitelist", function () {
 
         whitelist.initialize(accessList, hasGlobalAccess);
         expect(whitelist.isFeatureAllowed("http://www.google.com", "blackberry.app")).toEqual(true);
+        expect(whitelist.isFeatureAllowed("http://www.goo.com", "blackberry.app")).toEqual(false);
+        expect(whitelist.isFeatureAllowed("http://www.google.com", "blackberry.io.file")).toEqual(false);
     });
+
+    it("can allow access to local URLs", function() {
+        var hasGlobalAccess = false,
+            accessList = [{
+                uri : "WIDGET_LOCAL",
+                allowSubDomain : false,
+                features : [{
+                    id : "blackberry.media.microphone",
+                    required : true,
+                    version : "2.0.0"
+                }]
+            }];
+
+        whitelist.initialize(accessList, hasGlobalAccess);
+        expect(whitelist.isAccessAllowed("local:///index.html")).toEqual(true);
+        expect(whitelist.isFeatureAllowed("local:///index.html", "blackberry.media.microphone")).toEqual(true);
+    });
+
 });
     
     
