90a8714cb442b45953ba34b1d335d2d05117a342
Rosa Tse <rtse@rim.com>
2011-11-03 10:11:27 -0400

    Call reset() the first thing in initialize() to clear states, Added new test

--- test/unit/whitelist.js
+++ test/unit/whitelist.js
@@ -4,28 +4,46 @@ describe("whitelist", function () {
     var whitelist = require(srcPath + 'policy/whitelist');
     
     it("can allow access to any domain using uri *", function () {
-        var accessList = null; /*[{
+        var hasGlobalAccess = true,
+            accessList = null; /*[{
             allowSubDomain: false,      
             uri: "*"
         }];*/
 
-        whitelist.initialize(accessList, true);
+        whitelist.initialize(accessList, hasGlobalAccess);
         expect(whitelist.isAccessAllowed('http://www.google.com')).toEqual(true);
         expect(whitelist.isAccessAllowed('http://www.msn.com')).toEqual(true);
         expect(whitelist.isAccessAllowed('http://www.cnn.com')).toEqual(true);
         expect(whitelist.isAccessAllowed('http://www.rim.com')).toEqual(true);
     });
 
-    it("can allow access to google.com without *", function () {
-        var accessList = [{
-            uri : "http://google.com",
-            allowSubDomain : true,
-            features : null
-        }];
+    it("can allow access to domain without *", function () {
+        var hasGlobalAccess = false,
+            accessList = [{
+                uri : "http://google.com",
+                allowSubDomain : true,
+                features : null
+            }];
 
-        whitelist.initialize(accessList, false);
+        whitelist.initialize(accessList, hasGlobalAccess);
         expect(whitelist.isAccessAllowed("http://www.google.com")).toEqual(true);
         expect(whitelist.isAccessAllowed("http://www.cnn.com")).toEqual(false);
+    });
+
+    it("can allow access to feature", function () {
+        var hasGlobalAccess = false,
+            accessList = [{
+                uri : "http://google.com",
+                allowSubDomain : true,
+                features : [{
+                    id : "blackberry.app",
+                    required : true,
+                    version : "1.0.0"
+                }]
+            }];
+
+        whitelist.initialize(accessList, hasGlobalAccess);
+        expect(whitelist.isFeatureAllowed("http://www.google.com", "blackberry.app")).toEqual(true);
     });
 });
     
