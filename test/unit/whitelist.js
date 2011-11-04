7e0afafa364604b5db4bfb04c89164e388404e26
Rosa Tse <rtse@rim.com>
2011-11-04 14:55:07 -0400

    Cleaned up whitelist code

--- test/unit/whitelist.js
+++ test/unit/whitelist.js
@@ -5,10 +5,7 @@ describe("whitelist", function () {
     
     it("can allow access to any domain using uri *", function () {
         var hasGlobalAccess = true,
-            accessList = null; /*[{
-            allowSubDomain: false,      
-            uri: "*"
-        }];*/
+            accessList = null;
 
         whitelist.initialize(accessList, hasGlobalAccess);
         expect(whitelist.isAccessAllowed('http://www.google.com')).toEqual(true);
@@ -17,7 +14,7 @@ describe("whitelist", function () {
         expect(whitelist.isAccessAllowed('http://www.rim.com')).toEqual(true);
     });
 
-    it("can allow access to domain without *", function () {
+    it("can allow access to whitelisted HTTP URL without *", function () {
         var hasGlobalAccess = false,
             accessList = [{
                 uri : "http://google.com",
@@ -29,8 +26,20 @@ describe("whitelist", function () {
         expect(whitelist.isAccessAllowed("http://www.google.com")).toEqual(true);
         expect(whitelist.isAccessAllowed("http://www.cnn.com")).toEqual(false);
     });
+    
+    it("can deny access to non-whitelisted HTTP URL", function () {
+        var hasGlobalAccess = false,
+            accessList = [{
+                uri : "http://google.com",
+                allowSubDomain : true,
+                features : null
+            }];
 
-    it("can allow access to feature", function () {
+        whitelist.initialize(accessList, hasGlobalAccess);
+        expect(whitelist.isAccessAllowed("http://www.cnn.com")).toEqual(false);
+    });    
+
+    it("can allow access to whitelisted feature for whitelisted HTTP URLs", function () {
         var hasGlobalAccess = false,
             accessList = [{
                 uri : "http://google.com",
@@ -44,27 +53,55 @@ describe("whitelist", function () {
 
         whitelist.initialize(accessList, hasGlobalAccess);
         expect(whitelist.isFeatureAllowed("http://www.google.com", "blackberry.app")).toEqual(true);
-        expect(whitelist.isFeatureAllowed("http://www.goo.com", "blackberry.app")).toEqual(false);
-        expect(whitelist.isFeatureAllowed("http://www.google.com", "blackberry.io.file")).toEqual(false);
+    });
+    
+    it("can deny access to non-whitelisted feature for whitelisted HTTP URLs", function () {
+        var hasGlobalAccess = false,
+            accessList = [{
+                uri : "http://google.com",
+                allowSubDomain : true,
+                features : [{
+                    id : "blackberry.app",
+                    required : true,
+                    version : "1.0.0"
+                }]
+            }];
+
+        whitelist.initialize(accessList, hasGlobalAccess);
+        expect(whitelist.isFeatureAllowed("http://www.google.com", "blackberry.io.file")).toEqual(false);    
     });
 
     it("can allow access to local URLs", function () {
         var hasGlobalAccess = false,
             accessList = [{
-                uri : "WIDGET_LOCAL",
+                uri : "WIDGET_LOCAL",	// packager always inserts a local access into access list
+                allowSubDomain : false
+            }];
+
+        whitelist.initialize(accessList, hasGlobalAccess);
+        expect(whitelist.isAccessAllowed("local:///index.html")).toEqual(true);
+        expect(whitelist.isAccessAllowed("local:///appDir/subDir/index.html")).toEqual(true);
+    });
+    
+    it("can allow access to whitelisted features for local URLs", function () {
+        var hasGlobalAccess = false,
+            accessList = [{
+                uri : "WIDGET_LOCAL",	// packager always inserts a local access into access list
                 allowSubDomain : false,
                 features : [{
                     id : "blackberry.media.microphone",
                     required : true,
                     version : "2.0.0"
+                }, {
+                    id : "blackberry.media.camera",
+                    required : true,
+                    version : "1.0.0"
                 }]
             }];
 
         whitelist.initialize(accessList, hasGlobalAccess);
-        expect(whitelist.isAccessAllowed("local:///index.html")).toEqual(true);
         expect(whitelist.isFeatureAllowed("local:///index.html", "blackberry.media.microphone")).toEqual(true);
+        expect(whitelist.isFeatureAllowed("local:///index.html", "blackberry.media.camera")).toEqual(true);
     });
 
 });
-    
-    
