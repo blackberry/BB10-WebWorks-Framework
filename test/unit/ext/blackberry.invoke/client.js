76a4d9300ecf2b2148b9c175062989a595717386
Adrian Lee <adrilee@rim.com>
2012-03-19 13:00:49 -0400

    Add blackberry.invoke.invoke unit tests

--- test/unit/ext/blackberry.invoke/client.js
+++ test/unit/ext/blackberry.invoke/client.js
@@ -1,6 +1,27 @@
-describe("browser.invoke client", function () {
+describe("blackberry.invoke client", function () {
     var client = require('../../../../ext/blackberry.invoke/client'),
         utils = require('../../../../lib/utils');
+
+    describe("appType", function () {
+        it("should return constant for appropriate appType", function () {
+            expect(client.APP_CAMERA).toEqual(4);
+            expect(client.APP_MAPS).toEqual(5);
+            expect(client.APP_BROWSER).toEqual(11);
+            expect(client.APP_MUSIC).toEqual(13);
+            expect(client.APP_PHOTOS).toEqual(14);
+            expect(client.APP_VIDEOS).toEqual(15);
+            expect(client.APP_APPWORLD).toEqual(16);
+        });
+    });
+
+    describe("Browser Invoke", function () {
+        var url = "http://www.google.com";
+        xit("invoke should performExec", function () {
+            spyOn(utils, "performExec").andReturn(true);
+            client.invoke(client.APP_BROWSER, new client.BrowserArguments(url));
+            expect(utils.performExec).toHaveBeenCalled();
+        });
+    });
 
     describe("BrowserArguments", function () {
         var url = "http://www.google.com", 
@@ -26,27 +47,6 @@ describe("browser.invoke client", function () {
             browserArguments = new client.BrowserArguments(url);
             
             expect(browserArguments.url).toEqual("http://WWW.GOOGLE.COM");
-        });
-    });
-
-    describe("appType", function () {
-        it("should return constant for appropriate appType", function () {
-            expect(client.APP_CAMERA).toEqual(4);
-            expect(client.APP_MAPS).toEqual(5);
-            expect(client.APP_BROWSER).toEqual(11);
-            expect(client.APP_MUSIC).toEqual(13);
-            expect(client.APP_PHOTOS).toEqual(14);
-            expect(client.APP_VIDEOS).toEqual(15);
-            expect(client.APP_APPWORLD).toEqual(16);
-        });
-    });
-
-    describe("invoke", function () {
-        var url = "http://www.google.com";
-        it("invoke should performExec", function () {
-            // spyOn(utils, "performExec");
-            // client.invoke(client.APP_BROWSER, new client.BrowserArguments(url));
-            // expect(utils.performExec).toHaveBeenCalled();
         });
     });
 
--- test/unit/ext/blackberry.invoke/index.js
+++ test/unit/ext/blackberry.invoke/index.js
@@ -1,25 +1,45 @@
-describe("browser.invoke index", function () {
+describe("blackberry.invoke index", function () {
     var index = require('../../../../ext/blackberry.invoke/index'),
-        jnext = require('../../../../lib/jnext'),
-        _jnext = {
-            invoke: jasmine.createSpy()
-        };
+        jnext = require('../../../../lib/jnext');
 
-    describe("invoke", function () {
+    describe("Browser Invoke", function () {
         var mock_args = {
                 appType: 11,
                 args: encodeURIComponent(JSON.stringify({
                     url: 'http://www.rim.com'
                 }))
+            },
+            expected_invokeArgs = {
+                id: 1,
+                cmd: 'Open',
+                params: '/pps/services/navigator/control?server 2'
             };
-        
-        xit("should call jnext", function () {
-            spyOn(jnext, "invoke").andReturn(_jnext);
-            //spyOn(objJSExt, "sendCmd").andCallFake();
-            index.invoke(function () {}, function () {}, mock_args);
 
-            expect(_jnext.invoke).toHaveBeenCalled();
-            // expect(_jnext.invoke).toHaveBeenCalledWith();
+        beforeEach(function () {
+            spyOn(jnext, "require").andReturn(true);
+            spyOn(jnext, "createObject").andReturn(1);
+            spyOn(jnext, "invoke").andReturn('Ok');
+        });
+        
+        it("should call jnext", function () {         
+            index.invoke(function () {}, function () {}, mock_args);
+            expect(jnext.invoke).toHaveBeenCalled();
+        });
+
+        it("should require pps", function () {         
+            index.invoke(function () {}, function () {}, mock_args);
+            expect(jnext.require).toHaveBeenCalledWith('pps');
+        });
+
+        it("should createObject pps.PPS", function () {         
+            index.invoke(function () {}, function () {}, mock_args);
+            expect(jnext.createObject).toHaveBeenCalledWith('pps.PPS');
+        });
+        
+        
+        it("should generate expected invokeArgs output", function () {
+            index.invoke(function () {}, function () {}, mock_args);            
+            expect(jnext.invoke).toHaveBeenCalledWith(expected_invokeArgs);
         });
     });
 });
