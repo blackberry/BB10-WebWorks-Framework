c48dba4f007db3679fbcbff6f3f37257a7281712
Sergey Golod <sgolod@rim.com>
2012-03-26 13:51:57 -0400

    Code refactored

--- ext/blackberry.event/index.js
+++ ext/blackberry.event/index.js
@@ -1,29 +1,37 @@
-var _event = require("lib/event"), _ppsUtils = require("lib/pps/ppsUtils"), 
-_actionMap = {
-    batterystatus: {
-        context: require("lib/pps/ppsEvents"),
-        event: "batterystatus",
-        trigger: function(args) {
-            _event.trigger("batterystatus", args);
-        }
-    }
-/*    ,
-    batterylow: {
-        context: "",
-        event: "batterylow",
-        trigger: function(args) {
-            _event.trigger("foo", args);
-        }
-    },
-    batterycritical: {
-        context: "",
-        event: "batterycritical",
-        trigger: function(args) {
-            _event.trigger("foo", args);
-        }
-    }
-*/    
-};
+var _event = require("lib/event"), _ppsUtils = require("lib/pps/ppsUtils"), _eventsMap = {
+	    batterystatus: {
+	    	eventName: "batterystatus",
+	        eventDetailsArr: [{
+	            path: "/pps/services/power/battery?wait,delta",
+	            fieldNameArr: [{
+	                eventName: "StateOfCharge",
+	                paramName: "level",
+	                formatValue: function(str) {
+	                    return parseInt(str)
+	                }
+	            }]
+	        }, {
+	            path: "/pps/services/power/charger?wait,delta",
+	            fieldNameArr: [{
+	                eventName: "ChargingState",
+	                paramName: "isPlugged",
+	                formatValue: function(str) {
+	                    return (str === "NC" ? false : true)
+	                }
+	            }]
+	        }],
+	        mode: 0
+	    }
+	}, 
+	_actionMap = {
+	    batterystatus: {
+	        context: require("lib/pps/ppsEvents"),
+	        event: _eventsMap.batterystatus,
+	        trigger: function(args) {
+	            _event.trigger("batterystatus", args);
+	        }
+	    }
+	};
 
 function callIfDefined(func, args) {
     if(func) {
@@ -33,28 +41,11 @@ function callIfDefined(func, args) {
 
 module.exports = {
     on: function(success, fail, args) {
-        // TODO string argument surrounded by %22, to be fixed
         var eventName = decodeURIComponent(args.eventName).replace(/\"/g, ""), action = _actionMap[eventName];
         _event.on(action);
-        /*
-         if(name != "") {
-         _ppsUtils.registerEvent(eventName, action.webviewTrigger);
-         callIfDefined(success, eventName + " : handler added");
-         } else {
-         callIfDefined(fail, id + ": name not valid");
-         }
-         */
     },
-    
     remove: function(success, fail, args) {
-        // TODO string argument surrounded by %22, to be fixed
-        var name = decodeURIComponent(args.eventName).replace(/\"/g, "");
-
-        if(name != "") {
-            _pps.unregisterEvent(name);
-            callIfDefined(success, name + " : handler removed");
-        } else {
-            callIfDefined(fail, name + ": name not valid");
-        }
+        var eventName = decodeURIComponent(args.eventName).replace(/\"/g, ""), action = _actionMap[eventName];
+        _event.remove(action);
     }
 }
