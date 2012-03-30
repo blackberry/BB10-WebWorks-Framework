158ae29f167f13898e17c609fa7806d0a671355f
Rosa Tse <rtse@rim.com>
2012-03-30 11:14:06 -0400

    Added all fields (still using performExec, will change later when other pull requests got merged into next)

--- ext/blackberry.app/client.js
+++ ext/blackberry.app/client.js
@@ -6,12 +6,40 @@ function requireLocal(id) {
 var _self = {},
     performExec = requireLocal('lib/utils').performExec; // uses lib/utils for require id, ../.. causes problems
 
-_self.__defineGetter__("name", function () {
-    return performExec("blackberry.app", "name");
-});
-
 _self.__defineGetter__("author", function () {
     return performExec("blackberry.app", "author");
+});
+
+_self.__defineGetter__("authorEmail", function () {
+    return performExec("blackberry.app", "authorEmail");
+});
+
+_self.__defineGetter__("authorURL", function () {
+    return performExec("blackberry.app", "authorURL");
+});
+
+_self.__defineGetter__("copyright", function () {
+    return performExec("blackberry.app", "copyright");
+});
+
+_self.__defineGetter__("description", function () {
+    return performExec("blackberry.app", "description");
+});
+
+_self.__defineGetter__("id", function () {
+    return performExec("blackberry.app", "id");
+});
+
+_self.__defineGetter__("license", function () {
+    return performExec("blackberry.app", "license");
+});
+
+_self.__defineGetter__("licenseURL", function () {
+    return performExec("blackberry.app", "licenseURL");
+});
+
+_self.__defineGetter__("name", function () {
+    return performExec("blackberry.app", "name");
 });
 
 _self.__defineGetter__("version", function () {
--- ext/blackberry.app/index.js
+++ ext/blackberry.app/index.js
@@ -1,15 +1,43 @@
 var config = require("lib/config"); // uses lib/config for require id, ../.. causes problems
 
 module.exports = {
-    author: function (success, fail, args) {
+    author: function (success, fail, args, env) {
         success(config.author);
     },
 
-    name: function (success, fail, args) {
+    authorEmail: function (success, fail, args, env) {
+        success(config.authorEmail);
+    },
+
+    authorURL: function (success, fail, args, env) {
+        success(config.authorURL);
+    },
+
+    copyright: function (success, fail, args, env) {
+        success(config.copyright);
+    },
+
+    description: function (success, fail, args, env) {
+        success(config.description);
+    },
+
+    id: function (success, fail, args, env) {
+        success(config.id);
+    },
+
+    license: function (success, fail, args, env) {
+        success(config.license);
+    },
+
+    licenseURL: function (success, fail, args, env) {
+        success(config.licenseURL);
+    },
+
+    name: function (success, fail, args, env) {
         success(config.name);
     },
 
-    version: function (success, fail, args) {
+    version: function (success, fail, args, env) {
         success(config.version);
     }
 };
