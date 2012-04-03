f83910223e2f0d464fb13588efbda5e5196d4353
Adrian Lee <adrilee@rim.com>
2012-04-03 09:55:26 -0400

    Fix functional test lint errors and added waitsFor to .jshintrc predef list

--- test/functional/blackberry.event.js
+++ test/functional/blackberry.event.js
@@ -1,8 +1,8 @@
-describe("blackberry.event", function() {
+describe("blackberry.event", function () {
     window.confirm("Please unplug unit from power source.");
 
-    describe("EventListener", function() {
-        it("should not be called when the EventListener is removed", function() {
+    describe("EventListener", function () {
+        it("should not be called when the EventListener is removed", function () {
             var onBatteryStatusChange = jasmine.createSpy("onBatteryStatusChange");
                 
             blackberry.event.addEventListener('batterystatus', onBatteryStatusChange);
@@ -10,7 +10,9 @@ describe("blackberry.event", function() {
 
             window.confirm("[Device]Please connect power source. [SIM]Change 'StateOfCharge' to battery PPS Object");
 
-            waitsFor(function () { return (onBatteryStatusChange.callCount == 0) } , "Battery event fired", 5000);
+            waitsFor(function () { 
+                return (onBatteryStatusChange.callCount === 0); 
+            }, "Battery event fired", 5000);
 
             runs(function () {
                 expect(onBatteryStatusChange).not.toHaveBeenCalled();
@@ -18,15 +20,17 @@ describe("blackberry.event", function() {
         });
     });
 
-    describe("batterystatus", function() {
-        it("should be called when the battery level changes", function() {
+    describe("batterystatus", function () {
+        it("should be called when the battery level changes", function () {
             var onBatteryStatusChange = jasmine.createSpy("onBatteryStatusChange");
                 
             blackberry.event.addEventListener('batterystatus', onBatteryStatusChange);
 
             window.confirm("[Device]Please drain the battery. [SIM]Change 'StateOfCharge' in battery PPS Object");
 
-            waitsFor(function () { return onBatteryStatusChange.callCount } , "Battery event never fired", 120000);
+            waitsFor(function () { 
+                return onBatteryStatusChange.callCount;
+            }, "Battery event never fired", 120000);
 
             runs(function () {
                 expect(onBatteryStatusChange).toHaveBeenCalled();
@@ -36,14 +40,16 @@ describe("blackberry.event", function() {
             });
         });
 
-        it("should be called when the power source is connected", function() {
+        it("should be called when the power source is connected", function () {
             var onBatteryStatusChange = jasmine.createSpy("onBatteryStatusChange");
                 
             blackberry.event.addEventListener('batterystatus', onBatteryStatusChange);
 
             window.confirm("[Device]Please plug in a power source. [SIM]Change 'ChargingState' to 'DC' in charger PPS Object");
 
-            waitsFor(function () { return onBatteryStatusChange.callCount } , "Battery event never fired", 15000);
+            waitsFor(function () { 
+                return onBatteryStatusChange.callCount;
+            }, "Battery event never fired", 15000);
 
             runs(function () {
                 expect(onBatteryStatusChange).toHaveBeenCalled();
@@ -52,14 +58,16 @@ describe("blackberry.event", function() {
             });
         });
 
-        it("should be called when the power source is disconnected", function() {
+        it("should be called when the power source is disconnected", function () {
             var onBatteryStatusChange = jasmine.createSpy("onBatteryStatusChange");
                 
             blackberry.event.addEventListener('batterystatus', onBatteryStatusChange);
 
             window.confirm("[Device]Please unplug a power source. [SIM]Change 'ChargingState' to 'NC' in charger PPS Object");
 
-            waitsFor(function () { return onBatteryStatusChange.callCount } , "Battery event never fired", 15000);
+            waitsFor(function () {
+                return onBatteryStatusChange.callCount;
+            }, "Battery event never fired", 15000);
 
             runs(function () {
                 expect(onBatteryStatusChange).toHaveBeenCalled();
