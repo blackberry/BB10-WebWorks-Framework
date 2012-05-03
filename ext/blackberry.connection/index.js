bec9959e435ff1463fcf4029a31bf53b3d4d3487
Rosa Tse <rtse@rim.com>
2012-05-03 17:43:31 -0400

    blackberry.connection implementation - not tested yet

---
+++ ext/blackberry.connection/index.js
@@ -0,0 +1,59 @@
+/*
+* Copyright 2012 Research In Motion Limited.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+var connection;
+
+module.exports = {
+    type: function (success, fail, args) {
+        success(connection.getType());
+    }
+};
+
+///////////////////////////////////////////////////////////////////
+// JavaScript wrapper for JNEXT plugin
+///////////////////////////////////////////////////////////////////
+
+JNEXT.Connection = function ()
+{
+    var self = this;
+
+    self.getType = function () {
+        var val = JNEXT.invoke(self.m_id, "getType");
+        return val;
+    };
+
+    self.getId = function () {
+        return self.m_id;
+    };
+
+    self.init = function ()
+    {
+        if (!JNEXT.require("connection")) {
+            return false;
+        }
+
+        self.m_id = JNEXT.createObject("connection.Connection");
+
+        if (self.m_id === "") {
+            return false;
+        }
+    };
+
+    self.m_id = "";
+
+    self.init();
+};
+
+connection = new JNEXT.Connection();
---
+++ ext/blackberry.connection/native/connection_js.cpp
@@ -0,0 +1,53 @@
+/*
+* Copyright 2012 Research In Motion Limited.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+#include "connection_js.h"
+
+Connection::Connection(const std::string& id) : m_id(id)
+{
+
+}
+
+char* onGetObjList()
+{
+	static char name[] = "Connection";
+	return name;
+}
+
+JSExt* onCreateObject(const std::string& className, const std::string& id)
+{
+	if (className != "Connection") {
+		return NULL;
+	}
+
+	return new Connection(id);
+}
+
+std::string Connection::InvokeMethod(const std::string& command)
+{
+	int index = command.find_first_of(" ");
+
+	string strCommand = command.substr(0, index);
+	string jsonObject = command.substr(index + 1, command.length());
+	string returnValue;
+
+	if (strCommand == "getType") {
+		ConnectionBPS *connection = new ConnectionBPS();
+		itoa(connection->GetConnectionType(), returnValue.c_str(), 10);
+	}
+
+	return returnValue;
+}
+
